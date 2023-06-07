fc4a6a9490734b66e094d0654ff38d6cf91b3f19
trung-2004 <110414167+trung-2004@users.noreply.github.com>
2023-06-07 14:30:04 +0700

    web and admin display

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,5 +7,32 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function admin_dashboard() {
+        return view("admin.dashboard");
+    }
+    public function admin_booking() {
+        return view("admin.admin-booking");
+    }
+    public function admin_cars() {
+        return view("admin.admin-cars");
+
+    }
+    public function admin_cartype() {
+        return view("admin.admin-cartype");
+    }
+    public function admin_brand() {
+        return view("admin.admin-brands");
+    }
+    public function admin_contactquery() {
+        return view("admin.admin-contactquery");
+    }
+    public function admin_customer() {
+        return view("admin.admin-customers");
+    }
+    public function admin_service() {
+        return view("admin.admin-service");
+    }
+    public function admin_incident() {
+        return view("admin.admin-incident");
+    }
 }
--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -11,27 +11,27 @@ class WebController extends Controller
         return view("welcome");
     }
     public function car_list() {
-        return view("web\car-list");
+        return view("web.car-list");
     }
     public function booking() {
-        return view("web\booking");
+        return view("web.booking");
     }
     public function about() {
-        return view("web\about-us");
+        return view("web.about-us");
     }
     public function contact() {
-        return view("web\contact");
+        return view("web.contact");
     }
     public function car_detail() {
-        return view("web\car-detail");
+        return view("web.car-detail");
     }
     public function myOrders() {
-        return view("web\account-booking");
+        return view("web.account-booking");
     }
     public function dashboard() {
-        return view("web\account-dashboard");
+        return view("web.account-dashboard");
     }
     public function profile() {
-        return view("web\account-profile");
+        return view("web.account-profile");
     }
 }
