eb466bcb260c0af7621bf1f89ba86373df99cadd
Hieunbm <zhuongdae@gmail.com>
2023-06-09 15:58:23 +0700

    register rebase

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,20 +15,20 @@
                                 <form id="form_register" class="form-border" method="post" action="{{route("register")}}">
                                     @csrf
                                     <div class="field-set">
-                                        <input type="text" name="name" class="form-control" placeholder="full name" />
+                                        <input type="text" name="name" class="form-control" placeholder="Full name" />
                                     </div>
                                     <br>
                                     <div class="field-set">
-                                        <input type="email" name="email" class="form-control" placeholder="email" />
+                                        <input type="email" name="email" class="form-control" placeholder="Email" />
                                     </div>
                                     <br>
                                     <br>
                                     <div class="field-set">
-                                        <input type="password" name="password" class="form-control" placeholder="password" />
+                                        <input type="password" name="password" class="form-control" placeholder="Password" />
                                     </div>
                                     <br>
                                     <div class="field-set">
-                                        <input type="password" name="password_confirmation" class="form-control" placeholder="retype password" />
+                                        <input type="password" name="password_confirmation" class="form-control" placeholder="Retype Password" />
                                     </div>
                                     <br>
                                     <div id="submit">
