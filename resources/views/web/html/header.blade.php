b0bf38e792d24cfa967ceb421bcf7c29c6e00018
trung-2004 <110414167+trung-2004@users.noreply.github.com>
2023-06-10 22:49:43 +0700

    header

--- resources/views/web/html/header.blade.php
+++ resources/views/web/html/header.blade.php
@@ -5,7 +5,8 @@
                 <div class="topbar-widget">
                     <div class="topbar-widget"><a href="#"><i class="fa fa-phone"></i>+208 333 9296</a></div>
                     <div class="topbar-widget"><a href="#"><i class="fa fa-envelope"></i>contact@rentaly.com</a></div>
-                    <div class="topbar-widget"><a href="#"><i class="fa fa-clock-o"></i>Mon - Fri 08.00 - 18.00</a></div>
+                    <div class="topbar-widget"><a href="#"><i class="fa fa-clock-o"></i>Mon - Fri 08.00 - 18.00</a>
+                    </div>
                 </div>
             </div>
 
@@ -41,7 +42,6 @@
                         <ul id="mainmenu">
                             <li><a class="menu-item" href="{{url("/")}}">Home</a></li>
                             <li><a class="menu-item" href="{{url("/car-list")}}">Cars</a></li>
-                            <li><a class="menu-item" href="{{url("/booking")}}">Booking</a></li>
                             <li><a class="menu-item" href="{{url("/about")}}">About Us</a></li>
                             <li><a class="menu-item" href="{{url("/contact")}}">Contact</a></li>
                         </ul>
@@ -49,14 +49,30 @@
                     <div class="de-flex-col">
                         <div class="menu_side_area">
                             @guest()
-                                <a href="{{url("/login")}}"><i class="fa fa-user"></i> Login</a>
+                                <a class="btn-main" href="{{url("/login")}}"><i class="fa fa-user"></i> Login</a>
                             @endguest
                             @auth()
-                                <a href="{{url("/account-dashboard",[auth()->user()->id])}}"><i class="fa fa-user"></i> {{auth()->user()->name}}</a>
-                                <form action="{{route("logout")}}" method="post">
-                                    @csrf
-                                    <button type="submit"><i class="fa fa-arrow-right"></i></button>
-                                </form>
+                                <ul id="mainmenu">
+                                    <li><a href="#"><i class="fa fa-user"></i> {{auth()->user()->name}}</a>
+                                        <ul>
+                                            <li><a class="menu-item" href="{{url("/account-dashboard")}}">Dashboard</a>
+                                            </li>
+                                            <li><a class="menu-item" href="{{url("/account-profile")}}">My profile</a>
+                                            </li>
+                                            <li><a class="menu-item" href="{{url("/account-booking")}}">My orders</a>
+                                            </li>
+                                            <li>
+                                                <form action="{{route("logout")}}" method="post">
+                                                    @csrf
+                                                    <button style="border: none; background-color: white; width: 100%;"
+                                                            class="menu-item" type="submit"><i
+                                                            class="fa fa-arrow-right"></i>Logout
+                                                    </button>
+                                                </form>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
                             @endauth
                         </div>
                     </div>
