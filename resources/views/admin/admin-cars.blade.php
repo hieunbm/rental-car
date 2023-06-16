8664204e985c4923d668b2c99c7d50fca3e758da
Hieunbm <zhuongdae@gmail.com>
2023-06-16 16:31:46 +0700

    admin cars

--- resources/views/admin/admin-cars.blade.php
+++ resources/views/admin/admin-cars.blade.php
@@ -17,45 +17,65 @@
                             <th scope="col">Brand</th>
                             <th scope="col">Car Type</th>
                             <th scope="col">Name Car</th>
-                            <th scope="col">Status</th>
+                            <th scope="col">Fuel Type</th>
                             <th scope="col">Image</th>
+                            <th scope="col">Transmission</th>
+                            <th scope="col">Km Limit</th>
+                            <th scope="col">Model Year</th>
+                            <th scope="col">Status</th>
                             <th scope="col">Action</th>
                         </tr>
                         </thead>
                         <tbody>
+                        @foreach($cars as $item)
                         <tr>
-                            <td>1</td>
-                            <td><span class="badge bg-primary-transparent">Audi</span></td>
+                            <td>{{$item->id}}</td>
+                            <td><span class="badge bg-primary-transparent">{{$item->brands->name}}</span></td>
                             <td><a href="{{url("/admin/car-type")}}"><span
-                                        class="badge bg-primary-transparent">Minibus</span></a></td>
-                            <td>Audi A3</td>
-                            <td><span class="badge bg-success">Paid</span></td>
+                                        class="badge bg-primary-transparent">{{$item->carType->name}}</span></a></td>
+                            <td>{{$item->model}}</td>
+                            <td><span class="badge bg-success">{{$item->fuelType}}</span></td>
                             <td>
                                 <div class="avatar-list-stacked">
-                        <span class="avatar avatar-sm avatar-rounded">
-                            <img src="../dashboard/images/faces/2.jpg" alt="img">
-                        </span>
-                                    <span class="avatar avatar-sm avatar-rounded">
-                            <img src="../dashboard/images/faces/8.jpg" alt="img">
-                        </span>
-                                    <span class="avatar avatar-sm avatar-rounded">
-                            <img src="../dashboard/images/faces/2.jpg" alt="img">
-                        </span>
+                                    @if($item->images->count() > 3)
+                                        @foreach($item->images->take(3) as $tb)
+                                            <span class="avatar avatar-sm avatar-rounded">
+                                                <img src="{{$tb->thumbnail}}" alt="img">
+                                            </span>
+                                        @endforeach
                                     <a class="avatar avatar-sm bg-primary text-fixed-white avatar-rounded"
                                        href="javascript:void(0);">
-                                        +4
+                                        +{{$item->images->count()-3}}
                                     </a>
+                                    @else
+                                        @foreach($item->images as $tb)
+                                            <span class="avatar avatar-sm avatar-rounded">
+                            <img src="{{$tb->thumbnail}}" alt="img">
+                        </span>
+                                        @endforeach
+                                    @endif
                                 </div>
+                            </td>
+                            <td>
+                                {{$item->transmission}}
+                            </td>
+                            <td>
+                                {{$item->km_limit}}
+                            </td>
+                            <td>
+                                {{$item->modelYear}}
+                            </td>
+                            <td>
+                                {{$item->status}}
                             </td>
                             <td>
                                 <div class="hstack gap-2 fs-15">
-                                    <a href="javascript:void(0);" class="btn btn-icon btn-sm btn-success"><i
-                                            class="ri-download-2-line"></i></a>
-                                    <a href="javascript:void(0);" class="btn btn-icon btn-sm btn-info"><i
-                                            class="ri-edit-line"></i></a>
+                                    <a href="#" class="btn btn-icon btn-sm btn-info-transparent rounded-pill"><i class="ri-edit-line"></i></a>
+                                    <a href="#" class="btn btn-icon btn-sm btn-danger-transparent rounded-pill"><i class="ri-delete-bin-line"></i></a>
                                 </div>
                             </td>
                         </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
