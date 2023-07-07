f1c729859a8d5ea292dcd11619e8db7fb7a5c993
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2023-07-07 20:53:22 +0700

    update add gallery

--- resources/views/admin/admin-addcar-images.blade.php
+++ resources/views/admin/admin-addcar-images.blade.php
@@ -22,33 +22,90 @@
                     </li>
                 </ul>
 
-                <form action="{{url("admin/add-car/images")}}" method="POST" enctype="multipart/form-data">
+                <form id="my-dropzone" class="dropzone" action="{{url("admin/add-car/images")}}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="card-body">
                         <div class="row gy-4">
                             <div class="col-md-6">
+                               <div class="choose-car">
                                 <label for="input-placeholder" class="form-label">Choose Car</label>
-                                <select name="car_id" class="form-select default-pill" aria-label="Default select example">
-                                    @foreach($cars as $c)
-                                        <option value="{{$c->id}}">{{$c->model}} {{$c->license_plate}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-                            <div class="col-md-6">
-                                <label for="input-gallery" class="form-label">Thumbnail</label>
-                                <input type="file" name="gallery[]" class="form-control" id="input-gallery" multiple required>
-                            </div>
-
-                            <div class="col-12">
-                                <button type="submit" class="btn btn-primary">Add Gallery</button>
+                                    <select name="car_id" class="form-select default-pill" aria-label="Default select example">
+                                        @foreach($cars as $c)
+                                            <option value="{{$c->id}}">{{$c->model}} {{$c->license_plate}}</option>
+                                        @endforeach
+                                    </select>
+                               </div>
                             </div>
                         </div>
                     </div>
-
-
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <span id="error-message">Please choose a photo</span>
                 </form>
             </div>
         </div>
+    </div>
     <!-- End: Row -->
+<style>
+    .choose-car{
+        position: absolute;
+        margin-left: -600px;
+    }
+    .dropzone{
+        width: 500px;
+        margin-left: 600px;
+        position: relative;
+    }
+    .btn-primary{
+        margin-left: -580px;
+        margin-top: 60px;
+        position: absolute;
+    }
+    #error-message{
+        color: red;
+        display: none;
+        margin-top: -80px;
+        position: absolute;
+    }
+</style>
+
+    <script>
+        var myDropzone = new Dropzone("#my-dropzone", {
+            url: "{{url("admin/add-car/images")}}",
+            paramName: "thumbnail[]", // Tên trường tệp tin
+            maxFilesize: 2, // Kích thước tệp tối đa (MB)
+            maxFiles: 7, // Số lượng tệp tối đa được chấp nhận
+            acceptedFiles: ".jpeg,.jpg,.png", // Loại tệp được chấp nhận
+            parallelUploads: 7, // Số lượng tệp được tải lên cùng một lúc
+            autoProcessQueue: false, // Không tự động tải lên khi chọn tệp
+            dictDefaultMessage: "Detailed photo upload",
+            init: function() {
+                var dropzone = this;
+                var errorMessage = document.querySelector("#error-message");
+
+                // Khi người dùng nhấp vào nút Submit
+                document.querySelector("form").addEventListener("submit", function(e) {
+                    e.preventDefault();
+
+                    // Kiểm tra xem có tệp nào trong hàng đợi không
+                    if (dropzone.getQueuedFiles().length > 0) {
+                        dropzone.processQueue();
+                    } else {
+                        errorMessage.style.display = "block"; // Hiển thị thông báo lỗi
+                    }
+                });
+
+                // Xử lý khi tải lên thành công
+                dropzone.on("success", function(file, response) {
+                    console.log(response);
+                    // Chuyển hướng trang sau khi tải lên thành công
+                    window.location.href = "{{ url('/admin/cars') }}";
+                });
+
+                // Xử lý khi lỗi tải lên
+                dropzone.on("error", function(file, errorMessage) {
+                    console.error(errorMessage);
+                });
+            }
+        });
+    </script>
 @endsection
